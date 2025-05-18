347a0c638ccc0ed310636e633c73bf763cff043c
AderKonstantin <ki@aderk.tech>
2025-05-18 16:28:59 +0300

    Update route.ts and docker-compose.yml

--- app/api/views/route.ts
+++ app/api/views/route.ts
@@ -4,23 +4,23 @@ import { getRedisClient } from "../../../lib/redis";
 export const dynamic = 'force-dynamic'; // Отключаем кеширование
 
 export async function GET() {
-  try {
-    const redis = getRedisClient();
-    const projects = await redis.keys("projects:*:views");
-    const viewsEntries = await redis.mget(...projects);
-    
-    const viewsData = projects.reduce((acc, key, index) => {
-      const slug = key.split(":")[1];
-      acc[slug] = parseInt(viewsEntries[index] as string) || 0;
-      return acc;
-    }, {} as Record<string, number>);
+    try {
+        const redis = getRedisClient();
+        const projects = await redis.keys("projects:*:views");
+        const viewsEntries = await redis.mget(...projects);
 
-    return NextResponse.json(viewsData);
-  } catch (error) {
-    console.error("Redis error:", error);
-    return NextResponse.json(
-      { error: "Failed to fetch views" },
-      { status: 500 }
-    );
-  }
+        const viewsData = projects.reduce((acc, key, index) => {
+            const slug = key.split(":")[1];
+            acc[slug] = parseInt(viewsEntries[index] as string) || 0;
+            return acc;
+        }, {} as Record<string, number>);
+
+        return NextResponse.json(viewsData);
+    } catch (error) {
+        console.error("Redis error:", error);
+        return NextResponse.json(
+            { error: "Failed to fetch views" },
+            { status: 500 }
+        );
+    }
 }
